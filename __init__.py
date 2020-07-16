03700d4022ae982370c53c5065444ddd12e619ad
GabrielAmare <67603418+GabrielAmare@users.noreply.github.com>
2020-07-16 17:55:23 +0200

    Added todo websites for the data gather module

--- __init__.py
+++ __init__.py
@@ -1,3 +1,6 @@
 from .Wikitionnaire import Wikitionnaire
 from .Aidenet import Aidenet
 from .Wikipedia import Wikipedia
+
+# TODO: https://www.scribens.fr/  ce site à l'air vraiment bien foutu à voir !
+# TODO: Note : à voir https://www.cordial.fr/
